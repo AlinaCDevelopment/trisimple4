952e4c7372afccaa449c6b7a96b85c1e2f6c4c05
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2023-01-11 17:51:22 +0000

    layout de pending view

--- lib/views/pending_view.dart
+++ lib/views/pending_view.dart
@@ -1,7 +1,10 @@
 import 'package:app_4/services/l10n/app_localizations.dart';
+import 'package:app_4/widgets/themed_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import "package:intl/intl.dart";
+import 'package:path/path.dart';
+import '../../helpers/size_helper.dart';
 
 import '../screens/splash_screen.dart';
 import '../services/offline_service.dart';
@@ -22,28 +25,56 @@ class _PendingViewState extends State<PendingView> {
       child: FutureBuilder<List<Map<String, Object?>>>(
           future: OfflineService.instance.getPending(),
           builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data != null) {
+            /* if (snapshot.hasData && snapshot.data != null) {
               if (snapshot.data!.isEmpty) {
                 return Text(AppLocalizations.of(context).noPendingData);
-              }
-              return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) => ListTile(
-                        title: Column(
-                          children: [
-                            Text(
-                                "ID: ${snapshot.data![index]['id_bilhete'].toString()}"),
-                            Text(AppLocalizations.of(context)
-                                .pendingEntranceDate(DateFormat("dd-MM-yyyy")
-                                    .format(DateTime.parse(snapshot.data![index]
-                                            ['entrance']
-                                        .toString()))))
-                          ],
-                        ),
-                      ));
-            }
-            return const SplashScreen();
+              } */
+            return ListView.builder(
+                shrinkWrap: true,
+                itemCount: 1,
+                itemBuilder: (context, index) => ListTile(
+                      title: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(70),
+                            margin: EdgeInsets.only(top: 0),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border:
+                                  Border.all(width: 15, color: Colors.white),
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "4",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 80,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "Existem registos por comunicar...",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 70),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: SizeConfig.screenWidth * 0.08),
+                            child: ThemedButton(
+                                onTap: () => Navigator.pop(context),
+                                text: "Comunicar pendentes"),
+                          ),
+                        ],
+                      ),
+                    ));
+            /* return const SplashScreen(); */
           }),
     );
   }
